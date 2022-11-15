14246a2beed31842ee5f7d1b814ef9b8e5368391
Maulik sojitra <mauliksojitra322@gmail.com>
2022-11-15 22:36:18 +0530

    add login page

--- src/components/Sidebar/Layout.jsx
+++ src/components/Sidebar/Layout.jsx
@@ -1,7 +1,6 @@
 import React, { Fragment, useState } from 'react'
 import Footer from './Footer'
 import Header from './Header'
-import Sidebar from './Sidebar'
 
 const Layout = (props) => {
 
--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,13 +1,41 @@
-import React from 'react'
-import Footer from '../Sidebar/Footer'
+import React, { Fragment } from 'react'
+import { Button, Form } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+import '../../components/assets/css/Main.css'
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
-
-      <Footer />
-    </div>
+    <Fragment>
+      <div className='login-main p-3'>
+        <div className='banner-img'></div>
+        <div className='login-box'>
+          <div className='login-box-title'>
+            <h3>Login</h3>
+            <p>Enter your email and password to Login</p>
+          </div>
+          <Form className='login-form'>
+            <div className='input_field'>
+              <input class="input-search" type="text" required="required" />
+              <label>Enter Email</label>
+            </div>
+            <div className='input_field mt-4'>
+              <input class="input-search" type="password" required="required" />
+              <label>Enter Password</label>
+            </div>
+            <div className="d-flex align-items-center gap mt-4 remember">
+              <label className="input-toggle" for="login">
+                <input type="checkbox" id="login" name="switch" />
+                <span className="switch"></span>
+              </label>
+              <p className="mb-0">Remember me</p>
+            </div>
+            <Button className='mt-5 w-100 login-btn'>Login</Button>
+            <p className='last-def'>Don't have an account? <Link to="/"> Sign up</Link></p>
+          </Form>
+        </div>
+      </div>
+    </Fragment>
   )
 }
 
